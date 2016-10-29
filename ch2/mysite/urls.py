8128fe5a22fb9c5b8fe9bb490a1ac6282afb3177
Dokyung Lim <dklim7942@gmail.com>
2016-10-29 13:28:34 +0900

    Python_Web_1st_day

---
+++ ch2/mysite/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from django.contrib import admin
+
+from bookmark.views import *
+
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^bookmark/$', BookmarkLV.as_view(), name='index'),
+    url(r'^bookmark/(?P<pk>\d+)/$', BookmarkDV.as_view(), name='detail'),
+]
